3fe636f2e95214542ab1afa70612cb1bf6262d2b
einstein8612 <joey@joeyli.dev>
2021-11-12 19:19:00 +0100

    Init

---
+++ einsteinkeys.go
@@ -0,0 +1,24 @@
+package einsteinkeys
+
+import "github.com/fatih/color"
+
+func Validate(key string) {
+	color.Yellow(`  ______ _           _       _         _  __`)
+	color.Yellow(` |  ____(_)         | |     (_)       | |/ /`)
+	color.Yellow(` | |__   _ _ __  ___| |_ ___ _ _ __   | ' / ___ _   _ ___`)
+	color.Yellow(` |  __| | | '_ \/ __| __/ _ \ | '_ \  |  < / _ \ | | / __|`)
+	color.Yellow(` | |____| | | | \__ \ ||  __/ | | | | | . \  __/ |_| \__ \`)
+	color.Yellow(` |______|_|_| |_|___/\__\___|_|_| |_| |_|\_\___|\__, |___/`)
+	color.Yellow(`                                                 __/ |`)
+	color.Yellow(`                                                |___/`)
+
+	color.Yellow("Validating key: " + key)
+	//  ______ _           _       _         _  __
+	// |  ____(_)         | |     (_)       | |/ /
+	//
+	//
+	//
+	//
+	//
+	//
+}
