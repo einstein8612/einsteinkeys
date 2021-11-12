f0230b7d85fda2693927105e55824d3d31f248ee
einstein8612 <joey@joeyli.dev>
2021-11-12 20:31:17 +0100

    module name change

--- einsteinkeys.go
+++ einsteinkeys.go
@@ -3,7 +3,6 @@ package einsteinkeys
 import (
 	"crypto/sha256"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -42,61 +41,57 @@ func init() {
 }
 
 func Validate(keyString string) (err error) {
-	func() {
-		color.Cyan(`  ______ _           _       _         _  __`)
-		color.Cyan(` |  ____(_)         | |     (_)       | |/ /`)
-		color.Cyan(` | |__   _ _ __  ___| |_ ___ _ _ __   | ' / ___ _   _ ___`)
-		color.Cyan(` |  __| | | '_ \/ __| __/ _ \ | '_ \  |  < / _ \ | | / __|`)
-		color.Cyan(` | |____| | | | \__ \ ||  __/ | | | | | . \  __/ |_| \__ \`)
-		color.Cyan(` |______|_|_| |_|___/\__\___|_|_| |_| |_|\_\___|\__, |___/`)
-		color.Cyan(`                                                 __/ |`)
-		color.Cyan(`                                                |___/`)
+	color.Cyan(`  ______ _           _       _         _  __`)
+	color.Cyan(` |  ____(_)         | |     (_)       | |/ /`)
+	color.Cyan(` | |__   _ _ __  ___| |_ ___ _ _ __   | ' / ___ _   _ ___`)
+	color.Cyan(` |  __| | | '_ \/ __| __/ _ \ | '_ \  |  < / _ \ | | / __|`)
+	color.Cyan(` | |____| | | | \__ \ ||  __/ | | | | | . \  __/ |_| \__ \`)
+	color.Cyan(` |______|_|_| |_|___/\__\___|_|_| |_| |_|\_\___|\__, |___/`)
+	color.Cyan(`                                                 __/ |`)
+	color.Cyan(`                                                |___/`)
 
-		color.Cyan("Validating key: " + keyString)
+	color.Cyan("Validating key: " + keyString)
 
-		resp, err := http.Get("https://keys.joeyli.dev/keys?key=" + keyString)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
+	resp, err := http.Get("https://keys.joeyli.dev/keys?key=" + keyString)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-		if resp.StatusCode != http.StatusOK {
-			color.Red("This key is invalid")
-			return
-		}
+	if resp.StatusCode != http.StatusOK {
+		color.Red("This key is invalid")
+		return
+	}
 
-		var key Key
-		respBytes, err := io.ReadAll(resp.Body)
-		err = json.Unmarshal(respBytes, &key)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
+	var key Key
+	respBytes, err := io.ReadAll(resp.Body)
+	err = json.Unmarshal(respBytes, &key)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-		if !key.Enabled {
-			color.Red("This key is disabled")
-			return
-		}
+	if !key.Enabled {
+		color.Red("This key is disabled")
+		return
+	}
 
-		color.Cyan("This key is enabled")
-		color.Cyan("This software is licensed to: " + key.Licensee)
-		fmt.Println("")
-		checksum := checkSUM()
-		color.Cyan("The Sha256Checksum of this binary is:           " + checksum)
-		color.Cyan("The Sha256Checksum associated with this key is: " + key.Sha256Checksum)
-		if key.Sha256Checksum != checksum {
-			color.RedString("This binary's integrity is compromised. Contact Einstein if you believe this isn't true")
-			err = errors.New("compromised")
-			return
-		}
+	color.Cyan("This key is enabled")
+	color.Cyan("This software is licensed to: " + key.Licensee)
+	fmt.Println("")
+	checksum := checkSUM()
+	color.Cyan("The Sha256Checksum of this binary is:           " + checksum)
+	color.Cyan("The Sha256Checksum associated with this key is: " + key.Sha256Checksum)
+	if key.Sha256Checksum != checksum {
+		color.Red("This binary's integrity is compromised. Contact Einstein if you believe this isn't true")
+		return fmt.Errorf("Errored")
+	}
 
-		color.GreenString("This binary has been verified and it's integrity is intact")
-		color.Cyan("Continueing program in 3 seconds...")
-		time.Sleep(time.Second * 3)
+	color.GreenString("This binary has been verified and it's integrity is intact")
+	color.Cyan("Continueing program in 3 seconds...")
+	time.Sleep(time.Second * 3)
 
-		CallClear()
-	}()
-	time.Sleep(time.Second)
+	CallClear()
 	return
 }
 
